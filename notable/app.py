892b2b25f0e22af9dbaf81dc1da3b05a9fa18cd0
John McFarlane <john.mcfarlane@rockfloat.com>
2013-02-01 03:33:11 -0800

    Version 0.0.7

--- notable/app.py
+++ notable/app.py
@@ -23,7 +23,7 @@ from notable import bottle, db, editor
 
 # Constants, and help with template lookup
 host = 'localhost'
-version = '0.0.6b'
+version = '0.0.7'
 static = os.path.join(root, 'static')
 bottle.TEMPLATE_PATH.insert(0, os.path.join(root, 'views'))
 log = logging.getLogger(__name__)
