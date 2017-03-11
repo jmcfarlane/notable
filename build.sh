384b237849ae7caaf9fdbacd6c2fe4693a72331b
John McFarlane <john@rockfloat.com>
2017-03-10 22:25:27 -0800

    Initial release tooling

--- build.sh
+++ build.sh
@@ -2,17 +2,33 @@
 
 cd $(dirname $0)
 
+# The current version
+TAG="$(head -n1 CHANGELOG.md | grep -E -o 'v[^ ]+')"
+
+# Provide args for the program to display via -version
 flags="-X main.buildarch=$(go version | cut -f 4 -d' ')
        -X main.buildcompiler=$(go version | cut -f 3 -d' ')
        -X main.buildhash=$(git rev-parse --short HEAD)
        -X main.buildstamp=$(date -u '+%Y-%m-%dT%H:%M:%SZ')
        -X main.builduser=$(whoami)
-"
+       -X main.buildversion=${TAG}"
 
-mkdir -p target/{linux,macos}
-GOOS=darwin GOARCH=amd64 go build -ldflags "$flags" -o target/macos/notable
-GOOS=linux GOARCH=amd64 go build -ldflags "$flags" -o target/linux/notable
+# Clean house
+rm -rf target
+mkdir -p target/notable-${TAG}.darwin-amd64
+mkdir -p target/notable-${TAG}.linux-amd64
+
+# Build for each supported arch
+GOOS=darwin GOARCH=amd64 go build -ldflags "$flags" \
+    -o target/notable-${TAG}.darwin-amd64/notable
+GOOS=linux GOARCH=amd64 go build -ldflags "$flags" \
+    -o target/notable-${TAG}.linux-amd64/notable
+
+# Copy the license into each release binary
+cp LICENSE target/notable-${TAG}.darwin-amd64
+cp LICENSE target/notable-${TAG}.linux-amd64
 
 # Build zip files for hosting on github
-pushd target/macos && zip notable-macos.zip notable; popd
-pushd target/linux && zip notable-linux.zip notable; popd
+cd target
+zip -r notable-${TAG}.darwin-amd64.zip notable-${TAG}.darwin-amd64
+zip -r notable-${TAG}.linux-amd64.zip notable-${TAG}.linux-amd64
