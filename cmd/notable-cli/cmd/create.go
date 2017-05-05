0339f4401b0af534f4fc0be5010075f19f7e94d5
Sascha Andres <sascha.andres@outlook.com>
2017-05-05 19:03:28 +0200

    Created empty create command

---
+++ cmd/notable-cli/cmd/create.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new note",
+	Long:  `Add a new note to notable server`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		fmt.Println("create called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(createCmd)
+	createCmd.Flags().StringP("subject", "s", "", "Subject for the note")
+	createCmd.Flags().StringP("tags", "t", "", "Tags to associate")
+	createCmd.Flags().StringP("content", "c", "", "Content for the note")
+
+	viper.BindPFlag("create.subject", createCmd.Flags().Lookup("subject"))
+	viper.BindPFlag("create.tags", createCmd.Flags().Lookup("tags"))
+	viper.BindPFlag("create.content", createCmd.Flags().Lookup("content"))
+}
