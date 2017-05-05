6d967dced11a0c1ad9ab782e9c270c4fd8b74ae4
Sascha Andres <sascha.andres@outlook.com>
2017-05-05 19:47:31 +0200

    Added support for password

--- cmd/notable-cli/cmd/create.go
+++ cmd/notable-cli/cmd/create.go
@@ -23,15 +23,17 @@ var createCmd = &cobra.Command{
 		// TODO: Work your own magic here
 		log.Debugf("Creating a note on %s", viper.GetString("server"))
 		log.WithFields(log.Fields{
-			"Subject": viper.GetString("create.subject"),
-			"Tags":    viper.GetString("create.tags"),
-			"Content": viper.GetString("create.content"),
+			"Subject":  viper.GetString("create.subject"),
+			"Tags":     viper.GetString("create.tags"),
+			"Content":  viper.GetString("create.content"),
+			"Password": "Really?",
 		}).Debug("Provided values")
 
 		var note app.Note
 		note.Content = viper.GetString("create.content")
 		note.Tags = viper.GetString("create.tags")
 		note.Subject = viper.GetString("create.subject")
+		note.Password = viper.GetString("create.password")
 
 		data, err := json.MarshalIndent(note, "", "  ")
 		if err != nil {
@@ -50,8 +52,10 @@ func init() {
 	createCmd.Flags().StringP("subject", "s", "", "Subject for the note")
 	createCmd.Flags().StringP("tags", "t", "", "Tags to associate")
 	createCmd.Flags().StringP("content", "c", "", "Content for the note")
+	createCmd.Flags().StringP("password", "p", "", "Password to encrypt")
 
 	viper.BindPFlag("create.subject", createCmd.Flags().Lookup("subject"))
 	viper.BindPFlag("create.tags", createCmd.Flags().Lookup("tags"))
 	viper.BindPFlag("create.content", createCmd.Flags().Lookup("content"))
+	viper.BindPFlag("create.password", createCmd.Flags().Lookup("password"))
 }
